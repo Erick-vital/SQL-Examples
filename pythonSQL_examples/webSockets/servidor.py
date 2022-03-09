51aaed42e17fe89184c3ba86f477dc9f6c4f4c53
erick sanchez <71853038+Erick-vital@users.noreply.github.com>
2022-03-09 09:01:35 -0600

    validacion del lado del servidor y comandos agregado

--- pythonSQL_examples/webSockets/servidor.py
+++ pythonSQL_examples/webSockets/servidor.py
@@ -1,3 +1,4 @@
+import logging
 import os
 from dotenv import load_dotenv
 import hmac
@@ -5,24 +6,45 @@ import hashlib
 import asyncio
 from cProfile import run
 import websockets
+import json
+import pickle
 
 load_dotenv()
 private_key = os.getenv('p_k').encode()
+host = os.getenv('host')
+port = os.getenv('port')
 
-async def handler(websocket):
+async def main(websocket, path):
     mensaje = await websocket.recv()
-    print(f'<<< {mensaje}')
+    msg_json = json.loads(pickle.loads(mensaje))
+    sign = msg_json.pop("sign")
+    actual_digest = hmac.new(private_key, str(msg_json).encode(), hashlib.sha256).hexdigest()
 
 
-    sign = hmac.new(private_key, mensaje, hashlib.sha256).hexdigest()
+    # compara que las llaves sean correctas
+    if hmac.compare_digest(actual_digest, sign):
+        # ejecuta los comandos de la peticion
+        if msg_json["peticion"] == 'consulta':
+            os.system('python3 consultas.py --order > ouputs.txt')
+            os.system('python3 consultas.py --registros >> ouputs.txt')
+            logging.info('el output de peticion a sido escrito en el archivo outputs.tx')
+            print('comandos ejecutados con exito')
+        elif msg_json["peticion"] == 'crear':
+            os.system('python3 consultas.py --tabla')
+            os.system('python3 consultas.py --procedimiento')
+            logging.info('comandos ejecutados con exito')
+            print('comandos ejecutados con exito')
 
-    await websocket.send(sign)
-    #print(f'>>> {sign}')
+        logging.info('todo correcto en el servidor')
+        mensaje_servidor = ('todo correcto en el servidor')
+    else:
+        print('peligro posible corrupcion de datos')
+        logging.info('peligro posible corrupcion de datos')
+        mensaje_servidor = ('peligro posible corrupcion de datos')
 
-async def main():
-    async with websockets.serve(handler, 'localhost', 8765):
-        await asyncio.Future() # run forever
+    await websocket.send(mensaje_servidor)
 
-if __name__ == "__main__":
-    asyncio.run(main())
+start_server = websockets.serve(main, host, port)
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
     
