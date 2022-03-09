49053b92574bbb0a9e10c562b28ac6bf45bd6a1d
erick sanchez <71853038+Erick-vital@users.noreply.github.com>
2022-03-09 09:00:46 -0600

    .

--- pythonSQL_examples/webSockets/cliente.py
+++ pythonSQL_examples/webSockets/cliente.py
@@ -1,49 +1,38 @@
-import logging
-import os
-from dotenv import load_dotenv
-import hmac
-import hashlib
 import asyncio
-from cProfile import run
-import websockets
+import hashlib
+import hmac
 import json
+import logging
+import websockets
+from datetime import datetime
+from dotenv import load_dotenv
+import os 
 import pickle
+import json
 
 load_dotenv()
+
 private_key = os.getenv('p_k').encode()
+token = os.getenv('token')
 host = os.getenv('host')
 port = os.getenv('port')
 
-async def main(websocket, path):
-    mensaje = await websocket.recv()
-    msg_json = json.loads(pickle.loads(mensaje))
-    sign = msg_json.pop("sign")
-    actual_digest = hmac.new(private_key, str(msg_json).encode(), hashlib.sha256).hexdigest()
+parameters = {}
+parameters["token"] = token
+parameters["timestamp"] = str(datetime.now().isoformat())
+
+async def main():
+    async with websockets.connect(f"ws://{host}:{port}") as ws:
+        peticion = input('escribe tu peticion: ')
+        parameters["peticion"] = peticion
+        mensaje = str(parameters)
+        parameters["sign"] = hmac.new(private_key, mensaje.encode(), hashlib.sha256).hexdigest()
+        await ws.send(pickle.dumps(json.dumps(parameters)))
+
+        mensaje_del_servidor = await ws.recv()
+
+        logging.info(mensaje_del_servidor)
+        print(mensaje_del_servidor)
 
 
-    # compara que las llaves sean correctas
-    if hmac.compare_digest(actual_digest, sign):
-        # ejecuta los comandos de la peticion
-        if msg_json["peticion"] == 'consulta':
-            os.system('python3 consultas.py --order > ouputs.txt')
-            os.system('python3 consultas.py --registros >> ouputs.txt')
-            logging.info('el output de peticion a sido escrito en el archivo outputs.tx')
-            print('comandos ejecutados con exito')
-        elif msg_json["peticion"] == 'crear':
-            os.system('python3 consultas.py --tabla')
-            os.system('python3 consultas.py --procedimiento')
-            logging.info('comandos ejecutados con exito')
-            print('comandos ejecutados con exito')
-
-        logging.info('todo correcto en el servidor')
-        mensaje_servidor = ('todo correcto en el servidor')
-    else:
-        print('peligro posible corrupcion de datos')
-        logging.info('peligro posible corrupcion de datos')
-        mensaje_servidor = ('peligro posible corrupcion de datos')
-
-    await websocket.send(mensaje_servidor)
-
-start_server = websockets.serve(main, host, port)
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+asyncio.get_event_loop().run_until_complete(main())
