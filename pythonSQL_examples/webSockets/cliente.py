8a38b4ee5e2669a845f5791eafbc2ce6efb9b665
erick sanchez <71853038+Erick-vital@users.noreply.github.com>
2022-03-09 08:59:10 -0600

    alidacion del lado del servidor y comandos agregados

--- pythonSQL_examples/webSockets/cliente.py
+++ pythonSQL_examples/webSockets/cliente.py
@@ -1,36 +1,49 @@
-import asyncio
-import hashlib
-import hmac
-import urllib
-import websockets
-from datetime import datetime, timedelta
+import logging
+import os
 from dotenv import load_dotenv
-import os 
+import hmac
+import hashlib
+import asyncio
+from cProfile import run
+import websockets
+import json
+import pickle
 
 load_dotenv()
-
 private_key = os.getenv('p_k').encode()
-token = os.getenv('token')
-async def hello():
-    url = 'ws://localhost:8765'
-    async with websockets.connect(url) as websocket:
+host = os.getenv('host')
+port = os.getenv('port')
 
-        parameters = {}
-        parameters['token'] = token
-        parameters['timestamp'] = str(datetime.now().isoformat())
+async def main(websocket, path):
+    mensaje = await websocket.recv()
+    msg_json = json.loads(pickle.loads(mensaje))
+    sign = msg_json.pop("sign")
+    actual_digest = hmac.new(private_key, str(msg_json).encode(), hashlib.sha256).hexdigest()
 
-        mensaje = str(parameters.items()).encode()
 
-        actual_digest = hmac.new(private_key, mensaje, hashlib.sha256).hexdigest()
-        print(f'>>> actual_di: {actual_digest}')
+    # compara que las llaves sean correctas
+    if hmac.compare_digest(actual_digest, sign):
+        # ejecuta los comandos de la peticion
+        if msg_json["peticion"] == 'consulta':
+            os.system('python3 consultas.py --order > ouputs.txt')
+            os.system('python3 consultas.py --registros >> ouputs.txt')
+            logging.info('el output de peticion a sido escrito en el archivo outputs.tx')
+            print('comandos ejecutados con exito')
+        elif msg_json["peticion"] == 'crear':
+            os.system('python3 consultas.py --tabla')
+            os.system('python3 consultas.py --procedimiento')
+            logging.info('comandos ejecutados con exito')
+            print('comandos ejecutados con exito')
 
-        await websocket.send(mensaje)
+        logging.info('todo correcto en el servidor')
+        mensaje_servidor = ('todo correcto en el servidor')
+    else:
+        print('peligro posible corrupcion de datos')
+        logging.info('peligro posible corrupcion de datos')
+        mensaje_servidor = ('peligro posible corrupcion de datos')
 
-        sign = await websocket.recv()
-        if hmac.compare_digest(actual_digest, sign):
-            print('todo correcto')
-        else:
-            print('PELIGRO: posible corrupcion de datos')
-        print(f'<<<  recibido: {sign}')
-if __name__ == '__main__':
-    asyncio.run(hello())
+    await websocket.send(mensaje_servidor)
+
+start_server = websockets.serve(main, host, port)
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
